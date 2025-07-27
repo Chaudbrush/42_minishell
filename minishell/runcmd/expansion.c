86de49ccbdb5061ef23a49259da5d912b63d4e82
zainanz <zainanzaher09@gmail.com>
2025-07-27 14:25:43 +0100

    Expansion logic implemented

--- minishell/runcmd/expansion.c
+++ minishell/runcmd/expansion.c
@@ -1,24 +1,5 @@
 #include "runcmd.h"
 
-// put it in libft
-
-// char	*ft_strdup(char *src, char *dest)
-// {
-// 	int	src_len;
-// 	int	i;
-
-// 	i = 0;
-// 	if (!src || !dest)
-// 		return (NULL);
-// 	src_len = ft_strlen(src);
-// 	dest = malloc(src_len + 1);
-// 	while (*src)
-// 		dest[i++] = *src++;
-// 	dest[i] = 0;
-// 	return (dest);
-// }
-
-
 int is_expandable(char *str)
 {
 	while (*str)
@@ -32,6 +13,16 @@ int is_expandable(char *str)
 	return (0);
 }
 
+char	*extract_val(char *str)
+{
+	if (!str)
+		return (NULL);
+	while (*str && *str != '=')
+		str++;
+	str++;
+	return (str);
+}
+
 char	*search_envp(char *start, char *end)
 {
 	char	*str;
@@ -39,6 +30,8 @@ char	*search_envp(char *start, char *end)
 	t_envp	*env;
 
 	env = NULL;
+	if (!start || !end)
+		return (NULL);
 	str = safe_malloc(((end - start) + 1) * sizeof(char));
 	i = 0;
 	while (start < end)
@@ -51,7 +44,7 @@ char	*search_envp(char *start, char *end)
 	free(str);
 	if (!env)
 		return (NULL);
-	return (env->data);
+	return (extract_val(env->data));
 }
 
 char	*expand_dollar(char **src, int *i)
@@ -61,7 +54,7 @@ char	*expand_dollar(char **src, int *i)
 
 	str = NULL;
 	j = 0;
-	while (!char_presence((*src)[j], " \t\r\n\v")
+	while (!char_presence((*src)[j], " \t\r\n\v\"")
 			&& !check_illegal((*src)[j]) && (*src)[j])
 		j++;
 	str = search_envp(*src, *src + j);
@@ -116,12 +109,82 @@ int	get_expansion_len(char *src)
 	return (i);
 }
 
-void	perform_expansion(char *src, char *dest)
+int	append_var(char *str, char *dest)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	return (i);
+}
+
+void	copy_expansion(char *src, char **destination, int src_size)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	*destination = safe_malloc(sizeof(char) * (src_size + 1));
+	dest = *destination;
+	while (*src)
+	{
+		if (*src == '$')
+		{
+			src++;
+			i += append_var(expand_dollar(&src, NULL), dest + i);
+			continue ;
+		}
+		if (*src == '\'')
+		{
+			src++;
+			while (*src != '\'' && *src)
+			{
+				dest[i++] = *src;
+				src++;
+			}
+			if (*src)
+				src++;
+			continue ;
+		}
+		if (*src == '\"')
+		{
+			src++;
+			while (*src != '\"' && *src)
+			{
+				if (*src == '$')
+				{
+					src++;
+					i += append_var(expand_dollar(&src, NULL), dest + i);
+					continue ;
+				}
+				dest[i++] = *src;
+				src++;
+			}
+			src++;
+			continue ;
+		}
+		dest[i] = src[i]; 
+		i++;
+	}
+	dest[i] = 0;
+}
+
+void	perform_expansion(char *src, char **dest)
 {
 	int i;
 
-	(void)dest;
+	if (!src)
+		return ;
 	i = get_expansion_len(src);
+	printf("len is %i\n", i);
+	copy_expansion(src, dest, i);
+	printf("dest: %s\n", *dest);
 }
 
 char	**expansion(t_execcmd *execcmd)
@@ -130,11 +193,11 @@ char	**expansion(t_execcmd *execcmd)
 	int		i;
 
 	i = 0;
-	strs = safe_malloc(execcmd->size + 1);
+	strs = safe_malloc(sizeof(char *) * (execcmd->size + 1));
 	while (execcmd->argv[i])
 	{
 		if (is_expandable(execcmd->argv[i]))
-			perform_expansion(execcmd->argv[i], strs[i]);
+			perform_expansion(execcmd->argv[i], &strs[i]);
 		else
 			strs[i] = ft_strdup(execcmd->argv[i]);
 		i++;
