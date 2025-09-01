e94928382c692324a44f2aa2d21deaeca1009839
zali <zainanzaher09@gmail.com>
2025-09-01 12:42:46 +0100

    fixed norm errors, seperated builtin exec into a df file

---
+++ runcmd/builtin_exec.c
@@ -0,0 +1,69 @@
+#include "../includes/runcmd.h"
+
+static int	builtin_parent(char *str)
+{
+	if (!ft_strcmp(str, "cd") || !ft_strcmp(str, "exit")
+		|| !ft_strcmp(str, "unset") || !ft_strcmp(str, "export"))
+		return (1);
+	return (0);
+}
+
+static	void	built_in_exec_fork(t_exec *exec, t_cmd *cmd,
+				char **expanded_argv)
+{
+	free(exec->argv);
+	exec->builtin_heredoc = 1;
+	exec->argv = expanded_argv;
+	preprocess_heredoc(cmd);
+	redir_recursive(cmd, NULL);
+	exit_frees(cmd, shell()->envp_l, NULL, EXIT_SUCCESS);
+}
+
+static int	builtin_exec_check(t_cmd *cmd, char **expanded_argv,
+				t_exec *exec, t_cmd *temp)
+{
+	int	exit_val;
+
+	if (*expanded_argv && builtin_parent(*expanded_argv))
+	{
+		if (temp->type == REDIR)
+		{
+			if (safe_fork() == 0)
+				built_in_exec_fork(exec, cmd, expanded_argv);
+			waitpid(-1, &exit_val, 0);
+			reset_child_flag(exit_val);
+			if (exit_val != 0)
+			{
+				clear_av(expanded_argv);
+				free_trees(cmd);
+				return (1);
+			}
+		}
+		builtin_call(expanded_argv);
+		free_trees(cmd);
+		return (1);
+	}
+	return (0);
+}
+
+int	exec_builtin(t_cmd *cmd)
+{
+	t_exec	*exec;
+	t_cmd	*temp;
+	char	**expanded_argv;
+
+	temp = cmd;
+	if (temp->type == REDIR)
+	{
+		while (((t_redir *)temp)->link->type == REDIR)
+			temp = ((t_redir *)temp)->link;
+		exec = (t_exec *)(((t_redir *)temp)->link);
+	}
+	else
+		exec = (t_exec *)temp;
+	expanded_argv = expansion(exec);
+	if (builtin_exec_check(cmd, expanded_argv, exec, temp))
+		return (1);
+	clear_av(expanded_argv);
+	return (0);
+}
--- runcmd/runcmd.c
+++ runcmd/runcmd.c
@@ -1,11 +1,5 @@
 #include "../includes/runcmd.h"
 
-static int	builtin_parent(char *str);
-static void	reset_child_flag(int value);
-static int	run_cmd_builtin_check(t_cmd *cmd);
-static int	built_in_exec(t_cmd *cmd, char **expanded_argv,
-				t_exec *exec, t_cmd *temp);
-
 void	run_cmd(char *str)
 {
 	t_cmd	*cmd;	
@@ -22,7 +16,7 @@ void	run_cmd(char *str)
 	}
 	shell()->cmd = cmd;
 	if (cmd->type != PIPE)
-		if (run_cmd_builtin_check(cmd))
+		if (exec_builtin(cmd))
 			return ;
 	if (safe_fork() == 0)
 	{
@@ -35,73 +29,7 @@ void	run_cmd(char *str)
 	free_trees(cmd);
 }
 
-static int	built_in_exec(t_cmd *cmd, char **expanded_argv,
-				t_exec *exec, t_cmd *temp)
-{
-	int	exit_val;
-
-	if (*expanded_argv && builtin_parent(*expanded_argv))
-	{
-		if (temp->type == REDIR)
-		{
-			if (safe_fork() == 0)
-			{
-				free(exec->argv);
-				exec->builtin_heredoc = 1;
-				exec->argv = expanded_argv;
-				preprocess_heredoc(cmd);
-				redir_recursive(cmd, NULL);
-				free_trees(cmd);
-				clear_envp(shell()->envp_l);
-				exit(EXIT_SUCCESS);
-			}
-			waitpid(-1, &exit_val, 0);
-			reset_child_flag(exit_val);
-			if (exit_val != 0)
-			{
-				clear_av(expanded_argv);
-				free_trees(cmd);
-				return (1);
-			}
-		}
-		builtin_call(expanded_argv);
-		free_trees(cmd);
-		return (1);
-	}
-	return (0);
-}
-
-static int	run_cmd_builtin_check(t_cmd *cmd)
-{
-	t_exec	*exec;
-	t_cmd	*temp;
-	char	**expanded_argv;
-
-	temp = cmd;
-	if (temp->type == REDIR)
-	{
-		while (((t_redir *)temp)->link->type == REDIR)
-			temp = ((t_redir *)temp)->link;
-		exec = (t_exec *)(((t_redir *)temp)->link);
-	}
-	else
-		exec = (t_exec *)temp;
-	expanded_argv = expansion(exec);
-	if (built_in_exec(cmd, expanded_argv, exec, temp))
-		return (1);
-	clear_av(expanded_argv);
-	return (0);
-}
-
-static int	builtin_parent(char *str)
-{
-	if (!ft_strcmp(str, "cd") || !ft_strcmp(str, "exit")
-		|| !ft_strcmp(str, "unset") || !ft_strcmp(str, "export"))
-		return (1);
-	return (0);
-}
-
-static void	reset_child_flag(int value)
+void	reset_child_flag(int value)
 {
 	if (WIFEXITED(value))
 		value = WEXITSTATUS(value);
