556c869fb8f80e9012868b6cf99d8121368a24ac
vloureir <vloureir@student.42lisboa.com>
2025-09-15 11:01:14 +0100

    norm fix

--- signals/signals.c
+++ signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:39:33 by vloureir          #+#    #+#             */
-/*   Updated: 2025/09/15 10:53:17 by vloureir         ###   ########.fr       */
+/*   Updated: 2025/09/15 10:59:23 by vloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	sig_handler_parent(void)
 	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = parent_handler;
 	sa.sa_flags = SA_RESTART;
-	signal(SIGTSTP, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 	sigaction(SIGINT, &sa, NULL);
 }
@@ -46,7 +45,6 @@ void	sig_handler_doc(void)
 	ft_memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = doc_handler;
 	sa.sa_flags = SA_RESTART;
-	signal(SIGTSTP, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 	sigaction(SIGINT, &sa, NULL);
 }
